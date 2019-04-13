5545c426f886252f1dec64f10cd982d16a5be417
InnoFang <innofang@outlook.com>
2019-04-13 09:42:03 +0800

    :sparkles: Create a configuration file

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    # Which suffix code file do you want to count?
+    # more than as follow, also html, css, clj, lisp, etc.
+    'suffix': ['py', 'java', 'c', 'cpp', 'js', 'pde', 'kt', 'dart'],
+
+    # Ignore some directories or files which are not write by yourself
+    # but generate by the projects, just add what you want to add.
+    'ignore': ['out', 'venv', '.git', '.idea', 'build', 'target'],
+}

2b6ebd1391b2bca96b8b54cf96bf0779f0d0eff1
InnoFang <innofang@outlook.com>
2019-04-14 18:06:38 +0800

    :sparkles: implement the comment line counting

--- code-counter.py
+++ code-counter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import re
 import time
@@ -8,107 +10,136 @@ from config import config
 total_file_lines = 0
 total_code_lines = 0
 total_blank_lines = 0
+total_comment_lines = 0
 files_of_language = defaultdict(int)
 lines_of_language = {suffix: 0 for suffix in config['suffix']}
 
 ignore = config['ignore']
+comment_symbol = tuple(config['comment'])
 regex = '.*\.({})$'.format('|'.join(config['suffix']))
 pattern = re.compile(regex)
 
 
-def countFileLines(filename):
-    line_of_file = 0
-    line_of_code = 0
-    line_of_blank = 0
+def count(filename):
+    """
+    :param filename: the file you want to count
+    :return: file line, code line, blank line, comment line
+    """
+    file_line = 0
+    code_line = 0
+    blank_line = 0
+    comment_line = 0
     with open(filename, 'rb') as handle:
         for l in handle:
-            l_strip = l.strip()
-            line_of_file += 1
-            if not l_strip:
-                line_of_blank += 1
+            line = l.strip().decode("utf-8")
+            file_line += 1
+            if not line:
+                blank_line += 1
+            elif line.startswith(comment_symbol):
+                comment_line += 1
             else:
-                line_of_code += 1
-    return line_of_file, line_of_code, line_of_blank
+                code_line += 1
+    return file_line, code_line, blank_line, comment_line
 
 
-def formatOutput(file_path, f):
-    global total_file_lines, total_code_lines, total_blank_lines
+def format_output(file_path, f):
+    """
+    :param file_path: file path
+    :param f:  file
+    :return:
+    """
+    global total_file_lines, total_code_lines, total_blank_lines, total_comment_lines
     try:
         res = re.match(pattern, file_path)
         if res:
-            line_of_file, line_of_code, line_of_blank = countFileLines(file_path)
+            file_lines, code_lines, blank_lines, comment_lines = count(file_path)
             file_type = os.path.splitext(file_path)[1][1:]
             files_of_language[file_type] += 1
-            print('\t{:>10}  |  {:>13}  |  {:>13}  |  {:>13}  |  {}'.format(file_type, line_of_file, line_of_code,
-                                                                          line_of_blank, file_path), file=f)
-            total_file_lines += line_of_file
-            total_code_lines += line_of_code
-            total_blank_lines += line_of_blank
-            lines_of_language[file_type] += line_of_code
+            print('\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}  |  {}'
+                  .format(file_type, file_lines, code_lines, blank_lines, comment_lines, file_path), file=f)
+            total_file_lines += file_lines
+            total_code_lines += code_lines
+            total_blank_lines += blank_lines
+            total_comment_lines += comment_lines
+            lines_of_language[file_type] += code_lines
     except AttributeError as e:
         print(e)
 
 
-def listDir(path, f):
-    if os.path.isdir(path):
-        files = os.listdir(path)
+def search(p, f):
+    """
+    :param p: path
+    :param f: file
+    :return:
+    """
+    if os.path.isdir(p):
+        files = os.listdir(p)
         for file in files:
-            file_path = os.path.join(path, file)
+            file_path = os.path.join(p, file)
             if os.path.isdir(file_path):
                 if os.path.split(file_path)[-1] in ignore:
                     continue
-                listDir(file_path, f)
+                search(file_path, f)
             elif os.path:
-                file_path = os.path.join(path, file)
-                formatOutput(file_path, f)
-    elif os.path.isfile(path):
-        formatOutput(path, f)
+                file_path = os.path.join(p, file)
+                format_output(file_path, f)
+    elif os.path.isfile(p):
+        format_output(p, f)
 
 
-def search(path, input_path, output_path=None):
-    global total_file_lines, total_code_lines, total_blank_lines
+def main(p, i, o=None):
+    """
+    :param p: path
+    :param i: input file path
+    :param o: output file path
+    :return:
+    """
+    global total_file_lines, total_code_lines, total_blank_lines, total_comment_lines
 
-    f = open(output_path, 'w') if output_path else None
+    f = open(o, 'w') if o else None
 
     print('\n\t{}'.format("SEARCHING"), file=f)
     print("\t{}".format('=' * 20), file=f)
-    print('\t{:>10}  |  {:>13}  |  {:>13}  |  {:>13}  |  {}'.format("File Type", "Line of File", "Code of File",
-                                                                  "Blank of File", "File Path"), file=f)
+    print('\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}  |  {}'
+          .format("File Type", "Lines", "Code", "Blank", "Comment", "File Path"), file=f)
     print("\t{}".format('-' * 100), file=f)
 
-    if not path:
-        with open(input_path) as file:
+    if not p:
+        with open(i) as file:
             for l in file.readlines():
                 l_strip = l.strip()
                 if os.path.exists(l_strip):
-                    listDir(l_strip, f)
+                    search(l_strip, f)
                 else:
                     print('{} is not a validate path.'.format(l))
-    elif os.path.isdir(path):
-        listDir(path, f)
-    elif os.path.isfile(path):
-        if os.path.exists(path):
-            listDir(path, f)
+    elif os.path.isdir(p):
+        search(p, f)
+    elif os.path.isfile(p):
+        if os.path.exists(p):
+            search(p, f)
         else:
-            print('{} is not a validate path.'.format(path))
+            print('{} is not a validate path.'.format(p))
             exit(0)
     else:
-        print('{} is not a validate path.'.format(path))
+        print('{} is not a validate path.'.format(p))
         exit(0)
 
     print('\n\t{}'.format("RESULT"), file=f)
     print("\t{}".format('=' * 20), file=f)
     print("\t{:^25}|{:^15}|{:^15}|{:^15}|{:^15}"
-          .format("Item", "File Count", 'File Ratio', 'Code Count', 'Code Ratio'), file=f)
+          .format("Item", "File Number", 'File Ratio', 'Line Number', 'Line Ratio'), file=f)
     print("\t{}".format('-' * 90), file=f)
     print("\t{:<25}|{:^15}|{:^15}|{:^15}|{:^15}"
-          .format("Total line of files", '----', '----', total_file_lines, '100.00%'), file=f)
+          .format("Total file lines", '----', '----', total_file_lines, '100.00%'), file=f)
     print("\t{:<25}|{:^15}|{:^15}|{:^15}|{:^15}"
-          .format("Total line of codes", '----', '----',
+          .format("Total code lines", '----', '----',
                   total_code_lines, "%.2f%%" % (total_code_lines / total_file_lines * 100)), file=f)
     print("\t{:<25}|{:^15}|{:^15}|{:^15}|{:^15}"
-          .format("Total line of blank", '----', '----',
+          .format("Total blank lines", '----', '----',
                   total_blank_lines, "%.2f%%" % (total_blank_lines / total_file_lines * 100)), file=f)
+    print("\t{:<25}|{:^15}|{:^15}|{:^15}|{:^15}"
+          .format("Total comment lines", '----', '----',
+                  total_comment_lines, "%.2f%%" % (total_comment_lines / total_file_lines * 100)), file=f)
 
     total_files = 0
 
@@ -150,11 +181,10 @@ def args_parser():
 
 
 if __name__ == '__main__':
-
     path, input_path, output_path = args_parser()
 
     time_start = time.time()
-    search(path, input_path, output_path)
+    main(path, input_path, output_path)
     time_end = time.time()
 
     print('\n\tTotally cost {}s.'.format(time_end - time_start))
--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 config = {
     # Which suffix code file do you want to count?
     # more than as follow, also html, css, clj, lisp, etc.
-    'suffix': ['py', 'java', 'c', 'cpp', 'js', 'pde', 'kt', 'dart'],
+    'suffix': ('py', 'java', 'c', 'cpp', 'js', 'pde', 'kt', 'dart'),
 
-    # Ignore some directories or files which are not write by yourself
+    # the comment symbol, which can be judged whether the current line is a comment.
+    # However, if the current lines are between the comment symbol
+    # and there is not any comment symbol at the beginning of it, it will be misjudged.
+    # If the following comment symbol miss any other you want to add, you can do it by yourself
+    'comment': ('#', '//', '/*', '/**', '*', ':', ';'),
+
+    # Ignore some directories or files which are not write by yourself,
     # but generate by the projects, just add what you want to add.
-    'ignore': ['out', 'venv', '.git', '.idea', 'build', 'target', 'node_modules'],
+    'ignore': ('out', 'venv', '.git', '.idea', 'build', 'target', 'node_modules'),
 }

a9e4b78373b560b1d15dab236763dd0415338ac5
InnoFang <innofang@outlook.com>
2021-02-05 21:49:03 +0800

    :art: :recycle: factor and optimize

--- code-counter.py
+++ code-counter.py
@@ -8,8 +8,8 @@ import argparse
 import json
 from collections import defaultdict
 
-with open('./config.json', 'r') as f:
-    config = json.load(f)
+with open('./config.json', 'r') as config:
+    config = json.load(config)
 
 total_file_lines = 0
 total_code_lines = 0
@@ -56,10 +56,10 @@ def count(filename):
     return file_line, code_line, blank_line, comment_line
 
 
-def format_output(file_path, f):
+def format_output(file_path, output_file):
     """
     :param file_path: file path
-    :param f:  file
+    :param output_file:  file
     :return:
     """
     global total_file_lines, total_code_lines, total_blank_lines, total_comment_lines
@@ -70,7 +70,7 @@ def format_output(file_path, f):
             file_type = os.path.splitext(file_path)[1][1:]
             files_of_language[file_type] += 1
             print('\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}  |  {}'
-                  .format(file_type, file_lines, code_lines, blank_lines, comment_lines, file_path), file=f)
+                  .format(file_type, file_lines, code_lines, blank_lines, comment_lines, file_path), file=output_file)
             total_file_lines += file_lines
             total_code_lines += code_lines
             total_blank_lines += blank_lines
@@ -80,78 +80,78 @@ def format_output(file_path, f):
         print(e)
 
 
-def search(p, f):
+def search(input_path, output_file):
     """
-    :param p: path
-    :param f: file
+    :param input_path: path
+    :param output_file: file
     :return:
     """
-    if os.path.isdir(p):
-        files = os.listdir(p)
+    if os.path.isdir(input_path):
+        files = os.listdir(input_path)
         for file in files:
-            file_path = os.path.join(p, file)
+            file_path = os.path.join(input_path, file)
             if os.path.isdir(file_path):
                 if os.path.split(file_path)[-1] in ignore:
                     continue
-                search(file_path, f)
+                search(file_path, output_file)
             elif os.path:
-                file_path = os.path.join(p, file)
-                format_output(file_path, f)
-    elif os.path.isfile(p):
-        format_output(p, f)
+                file_path = os.path.join(input_path, file)
+                format_output(file_path, output_file)
+    elif os.path.isfile(input_path):
+        format_output(input_path, output_file)
 
 
-def main(p, i, o=None):
+def main(input_path, use_list=False, output_path=None):
     """
-    :param p: path
-    :param i: input file path
-    :param o: output file path
+    :param input_path: path
+    :param use_list: whether the path is the file that contains a list of file
+    :param output_path: output file path
     :return:
     """
     global total_file_lines, total_code_lines, total_blank_lines, total_comment_lines
 
-    f = open(o, 'w') if o else None
+    output_file = open(output_path, 'w') if output_path else None
 
-    print('\n\t{}'.format("SEARCHING"), file=f)
-    print("\t{}".format('=' * 20), file=f)
+    print('\n\t{}'.format("SEARCHING"), file=output_file)
+    print("\t{}".format('=' * 20), file=output_file)
     print('\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}  |  {}'
-          .format("File Type", "Lines", "Code", "Blank", "Comment", "File Path"), file=f)
-    print("\t{}".format('-' * 90), file=f)
+          .format("File Type", "Lines", "Code", "Blank", "Comment", "File Path"), file=output_file)
+    print("\t{}".format('-' * 90), file=output_file)
 
-    if not p:
-        with open(i) as file:
+    if use_list:
+        with open(input_path) as file:
             for l in file.readlines():
                 l_strip = l.strip()
                 if os.path.exists(l_strip):
-                    search(l_strip, f)
+                    search(l_strip, output_file)
                 else:
                     print('{} is not a validate path.'.format(l))
-    elif os.path.isdir(p):
-        search(p, f)
-    elif os.path.isfile(p):
-        if os.path.exists(p):
-            search(p, f)
+    elif os.path.isdir(input_path):
+        search(input_path, output_file)
+    elif os.path.isfile(input_path):
+        if os.path.exists(input_path):
+            search(input_path, output_file)
         else:
-            print('{} is not a validate path.'.format(p))
+            print('{} is not a validate path.'.format(input_path))
             exit(0)
     else:
-        print('{} is not a validate path.'.format(p))
+        print('{} is not a validate path.'.format(input_path))
         exit(0)
 
-    print('\n\t{}'.format("RESULT"), file=f)
-    print("\t{}".format('=' * 20), file=f)
+    print('\n\t{}'.format("RESULT"), file=output_file)
+    print("\t{}".format('=' * 20), file=output_file)
     print("\t{:<20}:{:>8} ({:>7})"
-          .format("Total file lines", total_file_lines, '100.00%'), file=f)
+          .format("Total file lines", total_file_lines, '100.00%'), file=output_file)
     print("\t{:<20}:{:>8} ({:>7})"
           .format("Total code lines",
-                  total_code_lines, "%.2f%%" % (total_code_lines / total_file_lines * 100)), file=f)
+                  total_code_lines, "%.2f%%" % (total_code_lines / total_file_lines * 100)), file=output_file)
     print("\t{:<20}:{:>8} ({:>7})"
           .format("Total blank lines",
-                  total_blank_lines, "%.2f%%" % (total_blank_lines / total_file_lines * 100)), file=f)
+                  total_blank_lines, "%.2f%%" % (total_blank_lines / total_file_lines * 100)), file=output_file)
     print("\t{:<20}:{:>8} ({:>7})"
           .format("Total comment lines",
-                  total_comment_lines, "%.2f%%" % (total_comment_lines / total_file_lines * 100)), file=f)
-    print(file=f)
+                  total_comment_lines, "%.2f%%" % (total_comment_lines / total_file_lines * 100)), file=output_file)
+    print(file=output_file)
 
     result['total']['code'] = total_code_lines
     result['total']['blank'] = total_blank_lines
@@ -163,19 +163,19 @@ def main(p, i, o=None):
         total_files += cnt
 
     print("\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}"
-          .format("Type", "Files", 'Ratio', 'Codes', 'Ratio'), file=f)
-    print("\t{}".format('-' * 65), file=f)
+          .format("Type", "Files", 'Ratio', 'Codes', 'Ratio'), file=output_file)
+    print("\t{}".format('-' * 65), file=output_file)
 
     for tp, cnt in files_of_language.items():
         code_line = lines_of_language[tp]
         print("\t{:>10}  |{:>10}  |{:>10}  |{:>10}  |{:>10}".format(
             tp, cnt, '%.2f%%' % (cnt / total_files * 100),
-            code_line, '%.2f%%' % (code_line / total_code_lines * 100)), file=f)
+            code_line, '%.2f%%' % (code_line / total_code_lines * 100)), file=output_file)
         result['code'][tp] = code_line
         result['file'][tp] = cnt
 
-    if f:
-        f.close()
+    if output_file:
+        output_file.close()
 
 
 def visualize():
@@ -229,38 +229,27 @@ def visualize():
 
 def args_parser():
     parser = argparse.ArgumentParser(prog="code-counter", description="Let's get count your code")
-    parser.add_argument('-i', '--input', dest='input',
+    
+    parser.add_argument('path', help="specify a file or directory path you want to search")
+    parser.add_argument('-l', '--list', dest='use_list', action='store_true',
                         help="the file contains a list of file path, "
                              "which can make you search more than one file or directory")
-    parser.add_argument('-p', '--path', dest='path',
-                        help="specify a file or directory path you want to search")
-    parser.add_argument('-o', '--output', dest='output',
-                        help="specify a output path if you want to store the result")
     parser.add_argument('-v', '--visual', dest='visual', action='store_true',
                         help="choose to whether to visualize the result")
+    parser.add_argument('-o', '--output', dest='output_path',
+                        help="specify a output path if you want to store the result")
 
-    args = parser.parse_args()
-
-    if not args.path and not args.input:
-        print('\033[0;31;0m[ERROR] Please specify a input: specify [-p PATH] or [-i INPUT]\033[0m')
-        print(parser.print_help())
-        exit(0)
-
-    if args.path and args.input:
-        print('\033[0;33;0m[WARN] Specify one input is enough,'
-              ' the option `[-p PATH]` will be cover the option `[-i INPUT]`\033[0m')
-
-    return args.path, args.input, args.output, args.visual
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    path, input_path, output_path, visual = args_parser()
+    args = args_parser()
 
     time_start = time.time()
-    main(path, input_path, output_path)
+    main(args.path, args.use_list, args.output_path)
     time_end = time.time()
 
     print('\n\tTotally cost {}s.'.format(time_end - time_start))
 
-    if visual:
+    if args.visual:
         visualize()

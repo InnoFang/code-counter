42b8910b07173ae164cc9e857b755b4ade78bd51
InnoFang <innofang@outlook.com>
2021-02-21 18:15:05 +0800

    :wrench: update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_readme(file_name):
 
 setup(name='code-counter',
       version=__import__('code_counter').__version__,
-      author="InnoFang",
+      author="Inno Fang",
       author_email="innofang@yeah.net",
       url='https://github.com/innofang/code-counter',  # homepage
       project_urls={
@@ -19,7 +19,7 @@ setup(name='code-counter',
         'Bug Reports': 'https://github.com/InnoFang/code-counter/issues',
       },
       description=short_desc,
-      long_description=read_readme('README.md'),  # for PyPI
+      long_description=read_readme('README.md'),
       packages=find_packages(),
       include_package_data=True,
       long_description_content_type="text/markdown",
@@ -27,7 +27,7 @@ setup(name='code-counter',
       install_requires = ["matplotlib", "numpy"],
       python_requires='>=3.5',
       classifiers=[
-        'Development Status :: 4 - Beta'
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Environment :: Console',
@@ -44,5 +44,5 @@ setup(name='code-counter',
               'codecount = code_counter.__main__:main'
           ]
       },
-      keywords='code line file count counter',
+      keywords='code count line file counter',
 )

5b17eebf9cd027273c35c05b3181e64bad292ed5
InnoFang <innofang@outlook.com>
2022-04-22 11:24:49 +0800

    :bug: fix the problem of adding duplicate data

--- code_counter/conf/config.py
+++ code_counter/conf/config.py
@@ -4,15 +4,20 @@
 import json
 import pkg_resources
 
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
 
 class Config:
 
     def __init__(self):
         conf = self.__load()
 
-        self.suffix = conf['suffix']
-        self.comment = conf['comment']
-        self.ignore = conf['ignore']
+        self.suffix = set(conf['suffix'])
+        self.comment = set(conf['comment'])
+        self.ignore = set(conf['ignore'])
 
     def invoke(self, args):
         if args.restore:
@@ -26,7 +31,7 @@ class Config:
             self.show()
 
     def show(self):
-        print(json.dumps(self.__dict__, indent=4))
+        print(json.dumps(self.__dict__, indent=4, cls=SetEncoder))
 
     def __confirm(self, tips):
         check = input(tips)
@@ -35,26 +40,26 @@ class Config:
     def __append_config(self, suffix_add, comment_add, ignore_add):
         if suffix_add:
             if self.__confirm("'suffix' will be appended with {} (y/n)".format(suffix_add)):
-                self.suffix.extend(suffix_add)
+                self.suffix.update(suffix_add)
         if comment_add:
             if self.__confirm("'comment' will be appended with {} (y/n)".format(comment_add)):
-                self.comment.extend(comment_add)
+                self.comment.update(comment_add)
         if ignore_add:
             if self.__confirm("'ignore' will be appended with {} (y/n)".format(ignore_add)):
-                self.ignore.extend(ignore_add)
+                self.ignore.update(ignore_add)
 
         self.__update()
 
     def __reset_config(self, suffix_reset, comment_reset, ignore_reset):
         if suffix_reset:
             if self.__confirm("'suffix' will be replaced with {} (y/n)".format(suffix_reset)):
-                self.suffix = suffix_reset
+                self.suffix = set(suffix_reset)
         if comment_reset:
             if self.__confirm("'comment' will be replaced with {} (y/n)".format(comment_reset)):
-                self.comment = comment_reset
+                self.comment = set(comment_reset)
         if ignore_reset:
             if self.__confirm("'ignore' will be replaced with {} (y/n)".format(ignore_reset)):
-                self.ignore = ignore_reset
+                self.ignore = set(ignore_reset)
 
         self.__update()
 
@@ -67,14 +72,14 @@ class Config:
     def __update(self):
         filename = pkg_resources.resource_filename(__name__, 'config.json')
         with open(filename, 'w') as config:
-            json.dump(self.__dict__, config, indent=4)
+            json.dump(self.__dict__, config, indent=4, cls=SetEncoder)
 
     def restore(self):
-        self.suffix = ["c", "cc", "clj", "cpp", "cs", "cu", "cuh", "dart", "go", "h",
-                       "hpp", "java", "jl", "js", "kt", "lisp", "lua", "pde", "m", "php",
-                       "py", "R", "rb", "rs", "rust", "sh", "scala", "swift", "ts", "vb"]
-        self.comment = ["#", "//", "/*", "*", "*/", ":", ";", '""""']
-        self.ignore = ["out", "venv", ".git", ".idea", "build", "target", "node_modules", ".vscode", "dist"]
+        self.suffix = {"c", "cc", "clj", "cpp", "cs", "cu", "cuh", "dart", "go", "h", "hpp", "java", "jl", "js", "kt",
+                       "lisp", "lua", "pde", "m", "php", "py", "R", "rb", "rs", "rust", "sh", "scala", "swift", "ts",
+                       "vb"}
+        self.comment = {"#", "//", "/*", "*", "*/", ":", ";", '""""'}
+        self.ignore = {"out", "venv", ".git", ".idea", "build", "target", "node_modules", ".vscode", "dist"}
 
-        if self.__confirm('Default configuration will be restored (y/n)?'):
+        if self.__confirm('The default configuration will be restored (y/n)?'):
             self.__update()

52b6603840b1a7673ac71850542892d730af791d
InnoFang <innofang@outlook.com>
2022-04-22 11:25:56 +0800

    :white_check_mark: pass tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # coding:utf8
 
-import sys
 import os
+import sys
 import unittest
 from unittest.mock import patch
-from code_counter.core.args import CodeCounterArgs
+
 from code_counter.conf.config import Config
+from code_counter.core.args import CodeCounterArgs
 
 test_path = os.path.abspath('.')
 bin_path = os.path.dirname(os.path.join(os.pardir, '..'))
@@ -16,11 +17,11 @@ app_path = os.path.join(lib_path, '__main__.py')
 
 class CodeCounterConfigTest(unittest.TestCase):
     def setUp(self):
-        self.default_suffix = ["c", "cc", "clj", "cpp", "cs", "cu", "cuh", "dart", "go", "h",
+        self.default_suffix = {"c", "cc", "clj", "cpp", "cs", "cu", "cuh", "dart", "go", "h",
                                "hpp", "java", "jl", "js", "kt", "lisp", "lua", "pde", "m", "php",
-                               "py", "R", "rb", "rs", "rust", "sh", "scala", "swift", "ts", "vb"]
-        self.default_comment = ["#", "//", "/*", "*", "*/", ":", ";", '""""']
-        self.default_ignore = ["out", "venv", ".git", ".idea", "build", "target", "node_modules", ".vscode", "dist"]
+                               "py", "R", "rb", "rs", "rust", "sh", "scala", "swift", "ts", "vb"}
+        self.default_comment = {"#", "//", "/*", "*", "*/", ":", ";", '""""'}
+        self.default_ignore = {"out", "venv", ".git", ".idea", "build", "target", "node_modules", ".vscode", "dist"}
 
     @patch('builtins.input')
     def test_Config_restore(self, mock_input):
@@ -59,9 +60,9 @@ class CodeCounterConfigTest(unittest.TestCase):
         self.assertTrue(args.has_config_args())
         config.invoke(args.config())
 
-        suffix = ['java', 'cpp', 'go', 'js', 'py']
-        comment = ['//', '#', '/**']
-        ignore = ['target', 'build', 'node_modules', '__pycache__']
+        suffix = {'java', 'cpp', 'go', 'js', 'py'}
+        comment = {'//', '#', '/**'}
+        ignore = {'target', 'build', 'node_modules', '__pycache__'}
 
         self.assertEqual(config.suffix, suffix)
         self.assertEqual(config.comment, comment)
@@ -87,9 +88,9 @@ class CodeCounterConfigTest(unittest.TestCase):
         self.assertTrue(args.has_config_args())
         config.invoke(args.config())
 
-        suffix = ['java', 'cpp', 'go', 'js', 'py']
-        comment = ['//', '#', '/**']
-        ignore = ['target', 'build', 'node_modules', '__pycache__']
+        suffix = {'java', 'cpp', 'go', 'js', 'py'}
+        comment = {'//', '#', '/**'}
+        ignore = {'target', 'build', 'node_modules', '__pycache__'}
 
         self.assertEqual(config.suffix, self.default_suffix)
         self.assertEqual(config.comment, comment)
@@ -115,9 +116,9 @@ class CodeCounterConfigTest(unittest.TestCase):
         self.assertTrue(args.has_config_args())
         config.invoke(args.config())
 
-        suffix = ['java', 'cpp', 'go', 'js', 'py']
-        comment = ['//', '#', '/**']
-        ignore = ['target', 'build', 'node_modules', '__pycache__']
+        suffix = {'java', 'cpp', 'go', 'js', 'py'}
+        comment = {'//', '#', '/**'}
+        ignore = {'target', 'build', 'node_modules', '__pycache__'}
 
         self.assertEqual(config.suffix, suffix)
         self.assertEqual(config.comment, self.default_comment)
@@ -143,13 +144,45 @@ class CodeCounterConfigTest(unittest.TestCase):
         self.assertTrue(args.has_config_args())
         config.invoke(args.config())
 
-        suffix = ['java', 'cpp', 'go', 'js', 'py']
-        comment = ['//', '#', '/**']
-        ignore = ['target', 'build', 'node_modules', '__pycache__']
+        suffix = {'java', 'cpp', 'go', 'js', 'py'}
+        comment = {'//', '#', '/**'}
+        ignore = {'target', 'build', 'node_modules', '__pycache__'}
 
         self.assertEqual(config.suffix, suffix)
         self.assertEqual(config.comment, comment)
         self.assertEqual(config.ignore, self.default_ignore)
+
+        config.restore()
+
+    @patch('builtins.input')
+    def test_Config_reset_duplicate(self, mock_input):
+        mock_input.side_effect = ['y', 'y', 'y', 'y', 'y']
+
+        config = Config()
+        config.restore()
+
+        options = ['python', app_path,
+                   'config',
+                   '--suffix-reset=py,py,py,py',
+                   '--comment-reset=#,#,#',
+                   '--ignore-reset=__pycache__,__pycache__']
+        sys.argv[1:] = options[2:]
+
+        args = CodeCounterArgs()
+        self.assertTrue(args.has_config_args())
+        config.invoke(args.config())
+
+        suffix = {'py'}
+        comment = {'#'}
+        ignore = {'__pycache__'}
+
+        self.assertEqual(len(config.suffix), 1)
+        self.assertEqual(len(config.comment), 1)
+        self.assertEqual(len(config.ignore), 1)
+
+        self.assertEqual(config.suffix, suffix)
+        self.assertEqual(config.comment, comment)
+        self.assertEqual(config.ignore, ignore)
 
         config.restore()
 

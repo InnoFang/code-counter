835f92f6a08b7d9e96be53129d1512e125c6f797
InnoFang <innofang@outlook.com>
2022-04-22 12:44:08 +0800

    :sparkles: support remove some values of config

--- code_counter/conf/config.py
+++ code_counter/conf/config.py
@@ -4,11 +4,13 @@
 import json
 import pkg_resources
 
+
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
             return list(obj)
         return json.JSONEncoder.default(self, obj)
+
 
 class Config:
 
@@ -23,10 +25,12 @@ class Config:
         if args.restore:
             self.restore()
         else:
-            if any([args.suffix_add, args.comment_add, args.ignore_add]):
-                self.__append_config(args.suffix_add, args.comment_add, args.ignore_add)
             if any([args.suffix_reset, args.comment_reset, args.ignore_reset]):
                 self.__reset_config(args.suffix_reset, args.comment_reset, args.ignore_reset)
+            if any([args.suffix_add, args.comment_add, args.ignore_add]):
+                self.__append_config(args.suffix_add, args.comment_add, args.ignore_add)
+            if any([args.suffix_del, args.comment_del, args.ignore_del]):
+                self.__remove_config(args.suffix_del, args.comment_del, args.ignore_del)
         if args.show_list:
             self.show()
 
@@ -37,29 +41,42 @@ class Config:
         check = input(tips)
         return check.strip().lower() == 'y'
 
+    def __reset_config(self, suffix_reset, comment_reset, ignore_reset):
+        if suffix_reset:
+            if self.__confirm("'suffix' will be replaced with {} . (y/n) ".format(suffix_reset)):
+                self.suffix = set(suffix_reset)
+        if comment_reset:
+            if self.__confirm("'comment' will be replaced with {} . (y/n) ".format(comment_reset)):
+                self.comment = set(comment_reset)
+        if ignore_reset:
+            if self.__confirm("'ignore' will be replaced with {} . (y/n) ".format(ignore_reset)):
+                self.ignore = set(ignore_reset)
+
+        self.__update()
+
     def __append_config(self, suffix_add, comment_add, ignore_add):
         if suffix_add:
-            if self.__confirm("'suffix' will be appended with {} (y/n)".format(suffix_add)):
+            if self.__confirm("'suffix' will be appended with {} . (y/n) ".format(suffix_add)):
                 self.suffix.update(suffix_add)
         if comment_add:
-            if self.__confirm("'comment' will be appended with {} (y/n)".format(comment_add)):
+            if self.__confirm("'comment' will be appended with {} . (y/n) ".format(comment_add)):
                 self.comment.update(comment_add)
         if ignore_add:
-            if self.__confirm("'ignore' will be appended with {} (y/n)".format(ignore_add)):
+            if self.__confirm("'ignore' will be appended with {} . (y/n) ".format(ignore_add)):
                 self.ignore.update(ignore_add)
 
         self.__update()
 
-    def __reset_config(self, suffix_reset, comment_reset, ignore_reset):
-        if suffix_reset:
-            if self.__confirm("'suffix' will be replaced with {} (y/n)".format(suffix_reset)):
-                self.suffix = set(suffix_reset)
-        if comment_reset:
-            if self.__confirm("'comment' will be replaced with {} (y/n)".format(comment_reset)):
-                self.comment = set(comment_reset)
-        if ignore_reset:
-            if self.__confirm("'ignore' will be replaced with {} (y/n)".format(ignore_reset)):
-                self.ignore = set(ignore_reset)
+    def __remove_config(self, suffix_del, comment_del, ignore_del):
+        if suffix_del:
+            if self.__confirm("'suffix' will remove {} . (y/n) ".format(suffix_del)):
+                self.suffix.difference_update(suffix_del)
+        if comment_del:
+            if self.__confirm("'comment' will remove {} . (y/n) ".format(comment_del)):
+                self.comment.difference_update(comment_del)
+        if ignore_del:
+            if self.__confirm("'ignore' will remove {} . (y/n) ".format(ignore_del)):
+                self.ignore.difference_update(ignore_del)
 
         self.__update()
 
@@ -81,5 +98,5 @@ class Config:
         self.comment = {"#", "//", "/*", "*", "*/", ":", ";", '""""'}
         self.ignore = {"out", "venv", ".git", ".idea", "build", "target", "node_modules", ".vscode", "dist"}
 
-        if self.__confirm('The default configuration will be restored (y/n)?'):
+        if self.__confirm('The default configuration will be restored. (y/n) '):
             self.__update()

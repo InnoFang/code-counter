8173b6e1bf80de06daa40f2c685639887fd67dc5
InnoFang <innofang@outlook.com>
2022-04-27 12:32:57 +0800

    :art: varify the status code of request result whether is 403

--- code_counter/tools/request.py
+++ code_counter/tools/request.py
@@ -4,14 +4,19 @@ session = requests.Session()
 
 
 def fetch(url, to_json=False):
-    for retry in range(20):
+    for retry in range(5):
         try:
-            res = session.get(url=url, verify=False, timeout=10)
+            res = session.get(url=url, timeout=10)
             if res.status_code == 200:
                 if to_json:
                     return res.json()
                 else:
                     return res.text
+            elif res.status_code == 403:
+                print('"API rate limit exceeded. update AccessToken please, and then retry.')
+                exit(1)
+            else:
+                print("fetch `{}` failed, error code {}.", url, res.status_code)
         except:
             print('Fetch "{}" error {} time(s):'.format(url, retry))
             print('\tA network problem occurs, re-request the URL automatically.')

b5a70027b98f430a983c6c3eaa53387319ae3932
InnoFang <innofang@outlook.com>
2022-04-28 23:17:06 +0800

    :sparkles: :art: :bug: Optimize requests to Github's and Gitee's repositories, and add handling of different status codes

--- code_counter/tools/request.py
+++ code_counter/tools/request.py
@@ -1,22 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8  -*-
+
 import requests
+from code_counter.conf.config import Config
 
 session = requests.Session()
 
 
+def is_gitee(url):
+    return url.lower().startswith('https://gitee.com')
+
+
+def get_access_token_for(url):
+    if is_gitee(url):
+        return Config().access_tokens.gitee
+    return Config().access_tokens.github
+
+
 def fetch(url, to_json=False):
-    for retry in range(5):
-        try:
+    access_token = get_access_token_for(url)
+    if is_gitee(url):
+        param = {'access_token': access_token}
+        if access_token:
+            res = session.get(url=url, params=param, timeout=10)
+        else:
             res = session.get(url=url, timeout=10)
-            if res.status_code == 200:
-                if to_json:
-                    return res.json()
-                else:
-                    return res.text
-            elif res.status_code == 403:
-                print('"API rate limit exceeded. update AccessToken please, and then retry.')
-                exit(1)
-            else:
-                print("fetch `{}` failed, error code {}.", url, res.status_code)
-        except:
-            print('Fetch "{}" error {} time(s):'.format(url, retry))
-            print('\tA network problem occurs, re-request the URL automatically.')
+    else:
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.github.v3.raw',
+            'Authorization': 'token ' + access_token
+        }
+        if access_token:
+            res = session.get(url=url, headers=headers, timeout=10)
+        else:
+            res = session.get(url=url, timeout=10)
+
+    if res.status_code == 200:
+        if to_json:
+            return res.json()
+        else:
+            return res.text
+    elif res.status_code in [401, 403]:
+        if res.status_code == 401:
+            print("Access token doesn't exist. Please re-enter or empty the access token.")
+        elif res.status_code == 403:
+            print('"API rate limit exceeded. Please update access token.')
+
+        if is_gitee(url):
+            Config().request_gitee_access_token()
+        else:
+            Config().request_github_access_token()
+        if access_token == get_access_token_for(url):
+            print("\nAccess token has not changed")
+            exit(1)
+    elif res.status_code == 404:
+        print("Not Found Project.")
+        exit(1)
+    else:
+        print("fetch `{}` failed, error code {}.", url, res.status_code)
